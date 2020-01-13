570a4ab7e368aaa39da60371a76d1a7f0815ed35
박대규 <mamma1234@gmail.com>
2020-01-13 17:23:26 +0900

    update

---
+++ src/components/ShopItemList.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ShopItem from './ShopItem';
+import { inject, observer } from 'mobx-react'; // 불러오기
+
+const items = [
+  {
+    name: '생수',
+    price: 850,
+  },
+  {
+    name: '신라면',
+    price: 900,
+  },
+  {
+    name: '포카칩',
+    price: 1500,
+  },
+  {
+    name: '새우깡',
+    price: 1000,
+  },
+];
+
+// const ShopItemList = () => {
+//   const itemList = items.map(item => <ShopItem {...item} key={item.name} />);
+//   return <div>{itemList}</div>;
+// };
+
+// **** onPut 함수 추가됨
+const ShopItemList = ({ onPut }) => {
+    const itemList = items.map(item => (
+      <ShopItem {...item} key={item.name} onPut={onPut} />
+    ));
+    return <div>{itemList}</div>;
+};
+// export default ShopItemList;
+
+// **** inject, observer 적용
+export default inject(({ market }) => ({
+    onPut: market.put,
+}))(observer(ShopItemList));
+
--- src/index.js
+++ src/index.js
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'mobx-react'; // MobX 에서 사용하는 Provider
+// import CounterStore from './stores/counter'; // 방금 만든 스토어 불러와줍니다.
+// import MarketStore from './stores/market';
+import RootStore from './stores';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const counter = new CounterStore(); // 스토어 인스턴스를 만들고
+// const market = new MarketStore();
+const root = new RootStore(); // *** 루트 스토어 생성
+
+
+ReactDOM.render(
+    // <Provider counter={counter} market={market}>
+    <Provider {...root}> {/* *** ...root 으로 스토어 모두 자동으로 설정 */}
+      {/* Provider 에 props 로 넣어줍니다. */}
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
